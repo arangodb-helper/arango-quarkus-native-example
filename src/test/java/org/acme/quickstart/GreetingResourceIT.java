eccadb651ee88b722fb22e802651a809adbacd6d
Michele Rastelli <michele@arangodb.com>
2023-02-23 19:47:14 +0100

    protocol test matrix

--- src/test/java/org/acme/quickstart/GreetingResourceIT.java
+++ src/test/java/org/acme/quickstart/GreetingResourceIT.java
@@ -3,6 +3,11 @@ package org.acme.quickstart;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
+class HttpJsonGreetingResourceIT extends HttpJsonGreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
+
+@QuarkusIntegrationTest
+class Http2JsonGreetingResourceIT extends Http2JsonGreetingResourceTest {
     // Execute the same tests but in packaged mode.
 }
--- src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -1,14 +1,13 @@
 package org.acme.quickstart;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@QuarkusTest
-class GreetingResourceTest {
-
+abstract class GreetingResourceTest {
     @Test
     void testVersionEndpoint() {
         String server = given()
@@ -18,8 +17,16 @@ class GreetingResourceTest {
                 .extract()
                 .jsonPath()
                 .getString("server");
-
         assertThat(server).isEqualTo("arango");
     }
+}
 
+@QuarkusTest
+@TestProfile(ProtocolProfile.HTTP_JSON.class)
+class HttpJsonGreetingResourceTest extends GreetingResourceTest {
+}
+
+@QuarkusTest
+@TestProfile(ProtocolProfile.HTTP2_JSON.class)
+class Http2JsonGreetingResourceTest extends GreetingResourceTest {
 }
